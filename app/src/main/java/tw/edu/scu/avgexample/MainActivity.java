04d6cc937f090e79b030c3963a35a6bd83b1061b
HaoXun <howardliu0907@gmail.com>
2024-05-30 20:27:12 +0800

    Logcat

--- app/src/main/java/tw/edu/scu/avgexample/MainActivity.java
+++ app/src/main/java/tw/edu/scu/avgexample/MainActivity.java
@@ -2,6 +2,7 @@ package tw.edu.scu.avgexample;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Logcat
+        Log.d("MainActivity", "onCreate");
 
         initButton();
     }
