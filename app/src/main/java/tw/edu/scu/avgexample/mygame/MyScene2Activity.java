6399ab069fc792958c1a0d8de698f1993e2cc5c0
HaoXun <howardliu0907@gmail.com>
2024-06-05 02:14:36 +0800

    Add Drawable Add MyScene2Activity

--- app/src/main/java/tw/edu/scu/avgexample/mygame/MyScene2Activity.java
+++ app/src/main/java/tw/edu/scu/avgexample/mygame/MyScene2Activity.java
@@ -2,7 +2,6 @@ package tw.edu.scu.avgexample.mygame;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,38 +11,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 import tw.edu.scu.avgexample.R;
 import tw.edu.scu.avgexample.framework.KWBaseSceneActivity;
-import tw.edu.scu.avgexample.framework.model.KWFirstPersonEventModel;
+import tw.edu.scu.avgexample.framework.model.KWCharacterModel;
+import tw.edu.scu.avgexample.framework.model.KWThirdPersonEventModel;
 import tw.edu.scu.avgexample.framework.utility.KWResourceUtils;
 
-public class MyScene1Activity extends KWBaseSceneActivity {
+public class MyScene2Activity extends KWBaseSceneActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_my_scene1);
+        setContentView(R.layout.activity_my_scene2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Log.d("MyScene1Activity", "onCreate");
-
     }
 
     @Override
     protected void initializeEvent () {
         super.initializeEvent();
 
-        Drawable background = KWResourceUtils.getDrawableByResourceId(this, R.drawable.desk);
+        KWCharacterModel character = new KWCharacterModel(this, "kw_female_001", "米奇");
 
-        KWFirstPersonEventModel event1 = new KWFirstPersonEventModel("我", "奇怪我怎麼醒在一個長得很像米奇妙妙屋的地方").setBackgroundDrawable(background);
-        KWFirstPersonEventModel event2 = new KWFirstPersonEventModel("我", "最近是不是發生了很多穿越事件，我不會也穿越了吧").setBackgroundDrawable(background);
+        KWThirdPersonEventModel event1 = new KWThirdPersonEventModel(character, "Hello!\n是我！米奇！");
+        KWThirdPersonEventModel event2 = new KWThirdPersonEventModel(character, "對了，要不要進我的妙妙屋！");
+
+        Drawable background = KWResourceUtils.getDrawableByResourceId(this, R.drawable.outdoor);
+        event1.setBackgroundDrawable(background);
 
         eventManager.addEvent(event1);
         eventManager.addEvent(event2);
-        eventManager.play("我的第一個事件");
+
+        eventManager.play("Scene2!");
 
     }
 
