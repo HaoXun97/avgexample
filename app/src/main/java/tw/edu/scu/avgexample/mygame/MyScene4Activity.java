6b2ffe2c241f511ca4d04d09eb41e359112f1285
HaoXun <howardliu0907@gmail.com>
2024-06-07 00:32:51 +0800

    Finish Scene 4(4a,4b,4c) (MyScene4Activity)

--- app/src/main/java/tw/edu/scu/avgexample/mygame/MyScene4Activity.java
+++ app/src/main/java/tw/edu/scu/avgexample/mygame/MyScene4Activity.java
@@ -1,5 +1,6 @@
 package tw.edu.scu.avgexample.mygame;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,10 +9,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import tw.edu.scu.avgexample.R;
 import tw.edu.scu.avgexample.framework.KWBaseActivity;
 import tw.edu.scu.avgexample.framework.KWBaseSceneActivity;
+import tw.edu.scu.avgexample.framework.model.KWCharacterModel;
+import tw.edu.scu.avgexample.framework.model.KWFirstPersonEventModel;
 import tw.edu.scu.avgexample.framework.model.KWFullScreenEventModel;
+import tw.edu.scu.avgexample.framework.model.KWOptionEventModel;
+import tw.edu.scu.avgexample.framework.model.KWPictureEventModel;
+import tw.edu.scu.avgexample.framework.model.KWThirdPersonEventModel;
+import tw.edu.scu.avgexample.framework.utility.KWResourceUtils;
 
 public class MyScene4Activity extends KWBaseSceneActivity {
 
@@ -38,12 +48,52 @@ public class MyScene4Activity extends KWBaseSceneActivity {
     protected void didFinishAllEvent(String eventIdentifier) {
         super.didFinishAllEvent(eventIdentifier);
         if ("Scene4_Start".equals(eventIdentifier)) {
-            KWFullScreenEventModel event = new KWFullScreenEventModel("SCENE4_START");
-            eventManager.addEvent(event);
+            Drawable background = KWResourceUtils.getDrawableByResourceId(this, R.drawable.door_a);
+            KWFullScreenEventModel event1 = new KWFullScreenEventModel("跟著米奇進入房間後看見房間還有一隻可愛的小狗");
+            event1.setBackgroundDrawable(background);
+            eventManager.addEvent(event1);
             eventManager.play("Scene4-1");
+        } else if ("Scene4-1".equals(eventIdentifier)) {
+            KWCharacterModel character = new KWCharacterModel(this, "test", "米奇");
+            Drawable background = KWResourceUtils.getDrawableByResourceId(this, R.drawable.mickey_paper);
+            Drawable picture = KWResourceUtils.getDrawableByResourceId(this, R.drawable.paper);
+
+            KWThirdPersonEventModel event1 = new KWThirdPersonEventModel(character, "這是今天我們的朋友送來寄放的小狗");
+            event1.setBackgroundDrawable(background);
+            KWPictureEventModel event2 = new KWPictureEventModel(picture, "米奇", "可以請你為她制定一個行程嗎？");
+
+            eventManager.addEvent(event1);
+            eventManager.addEvent(event2);
+            eventManager.play("Scene4-2");
+        } else if ("Scene4-2".equals(eventIdentifier)) {
+
+            ArrayList<String> optionArrayList = new ArrayList<>();
+            optionArrayList.add("陪他玩");
+            optionArrayList.add("洗澡");
+            optionArrayList.add("吃午飯");
+
+            KWCharacterModel character = new KWCharacterModel(this, "test", "米奇");
+
+            KWOptionEventModel event1 = new KWOptionEventModel("Scene4-3_Option", "選擇一個行程", optionArrayList);
+            KWThirdPersonEventModel event2 = new KWThirdPersonEventModel(character, "太好了！謝謝你為小狗制定了計畫，就讓我們度過這愉快的一天吧！");
+            KWFirstPersonEventModel event3 = new KWFirstPersonEventModel("我", "我認為只有我們可能沒辦法好好照顧好小狗");
+            KWThirdPersonEventModel event4 = new KWThirdPersonEventModel(character, "我覺得你說的有道理，那我們去拿 mouseketools 吧！");
+            KWThirdPersonEventModel event5 = new KWThirdPersonEventModel(character);
+            eventManager.addEvent(event1);
+            eventManager.addEvent(event2);
+            eventManager.addEvent(event3);
+            eventManager.addEvent(event4);
+            eventManager.addEvent(event5);
+            eventManager.play("Scene4-3");
+
+        } else if ("Scene4a".equals(eventIdentifier)) {
+            switchSceneActivity(MyScene4aActivity.class);
         }
-        else if ("Scene4-1".equals(eventIdentifier)) {
-            eventManager.play("Scene4_End");
+        else if ("Scene4b".equals(eventIdentifier)) {
+            switchSceneActivity(MyScene4bActivity.class);
+        }
+        else if ("Scene4c".equals(eventIdentifier)) {
+            switchSceneActivity(MyScene4cActivity.class);
         }
         else if ("Scene4_End".equals(eventIdentifier)) {
             finish();
@@ -54,6 +104,14 @@ public class MyScene4Activity extends KWBaseSceneActivity {
     @Override
     protected void onOptionSelected(String identifier, int index) {
         super.onOptionSelected(identifier, index);
-
+        if ("Scene4-3_Option".equals(identifier)) {
+            if (index == 0) {
+                eventManager.play("Scene4a");
+            } else if (index == 1) {
+                eventManager.play("Scene4b");
+            } else if (index == 2) {
+                eventManager.play("Scene4c");
+            }
+        }
     }
 }
